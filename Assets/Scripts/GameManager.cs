72a84cf98719680cfea136443555e799ffd210d9
Or Shtrauch <ororor2000@gmail.com>
2023-03-30 07:59:39 +0300

    new vars and logic in gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,12 @@ public class GameManager : MonoBehaviour
    private static GameManager instance;
 
     public static GameManager Instance { get; private set; }
+
+    public int pgaeCounter = 0;
+    public GameObject[] spawnLocations = new GameObject[15];
+    public int currentSpawnIndex = Random.Range(0, 15);
+
+    public GameObject PagePrefab;
 
     private void Awake()
     {
@@ -18,5 +25,28 @@ public class GameManager : MonoBehaviour
         {
             Instance = this;
         }
+    }
+
+    int ChooseSpawnLocation()
+    {
+        int index = currentSpawnIndex;
+        while (index == currentSpawnIndex)
+        {
+            index = Random.Range(0, spawnLocations.Length);
+        }
+
+        return index;
+    }
+
+    void SpawnPage()
+    {
+       currentSpawnIndex = ChooseSpawnLocation();
+
+       if (currentSpawnIndex > spawnLocations.Length || currentSpawnIndex < 0)
+       {
+            return;
+       }
+
+        Instantiate(PagePrefab, spawnLocations[currentSpawnIndex].transform);
     }
 }
